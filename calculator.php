6a2ceb6dbcb98840de461e8644a5eec88fa0e08f
Lijie Zhou <lijie@mail.sfsu.edu>
2017-01-02 10:28:02 -0800

    bill display after post

--- calculator.php
+++ calculator.php
@@ -1,6 +1,7 @@
 <html>
 <head>
     <title> Tip Calculator </title>
+    <!-- CSS part -->
     <style type="text/css">
     	body {background-color: fdffc3;}
     	#outer {
@@ -14,55 +15,48 @@
     	h1, #inner{
     		margin: 0 auto;
     		text-align: center;
-    		vertical-align: middle;
     	}
-    	
-
     </style>
 <body>
-<div id="outer" >
-	<h1>Tip Calculator</h1>
-	<div id="inner">
-    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
-    Bill Subtitle:$  <input type="Text" Name="Num1" value="" placeholder="0"><br>
-    Tip Percentage: 
-    <?php
-    $array = array(0.1,0.15,0.2);
-    foreach($array as $value){
-        
-    ?>
-    <input type="radio" name="tip_percentage" value="<?php echo $value; ?>" ><?php echo $value; ?><br>
-    <?php
-    }
-    ?>
- 
-    <input type="Submit" value="Calculate">
-    </form>
+	<div id="outer" >
+		<h1>Tip Calculator</h1>
+		<div id="inner">
+		    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+		    <!-- Get user input of Bill Subtitle, display default value as zero -->
+		    Bill Subtitle:$  <input type="Text" Name="Num1" placeholder="0" value="<?php echo isset($_POST['Num1']) ? $_POST['Num1'] : '' ?>"><br>
+		    Tip Percentage: 
+		    <!-- Use a PHP loop to output the three radio buttons -->
+		    <?php
+		    $array = array(0.1,0.15,0.2);
+		    foreach($array as $value){   
+		    ?>
+		    <input type="radio" name="tip_percentage" value="<?php echo $value; ?>" ><?php echo $value; ?><br>
+		    <?php
+		    }
+		    ?>
+		 	<!-- Submit Form -->
+		    <input type="Submit" value="Calculate">
+		    </form>
 
+		    <!-- Validate User Input, display corresponding error messages  -->
+			<?php
 
-<?php
+			if (count($_POST) > 0 && is_numeric($_POST["Num1"]) && ($_POST["Num1"] >0) && isset($_POST["tip_percentage"])){
 
+				$sum = $_POST["Num1"] * $_POST["tip_percentage"] + $_POST["Num1"];
+				echo "Tip is ".$_POST["Num1"]*$_POST["tip_percentage"]." and
+				total is $sum";
+			}
+			if (isset($_POST["Num1"]) && $_POST["Num1"] < 0){
+				echo "Subtitle should be greater than 0";
+			}
+			if (isset($_POST["Num1"]) && !is_numeric($_POST["Num1"])){
+				echo "Subtitle should be a numeric number.";
+			}
+			?>
 
-
-	if (count($_POST) > 0 && is_numeric($_POST["Num1"]) && ($_POST["Num1"] >0) && isset($_POST["tip_percentage"])){
-
-		
-		$sum = $_POST["Num1"] * $_POST["tip_percentage"] + $_POST["Num1"];
-		// their sum is diplayed as
-		echo "Tip is ".$_POST["Num1"]*$_POST["tip_percentage"]." and
-		total is $sum";
-	}
-	if (isset($_POST["Num1"]) && $_POST["Num1"] < 0){
-		echo "Subtitle should be greater than 0";
-	}
-	if (isset($_POST["Num1"]) && !is_numeric($_POST["Num1"])){
-		echo "Subtitle should be a numeric number.";
-	}
-	
-	
-?>
-</div>
-</div>
+		</div>
+	</div>
 
 </body>
 </html>
